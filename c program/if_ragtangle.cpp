13c61369b9225e7d295279687725234de4b6c0e4
jenil kakadiya <jenilkakadiya2602@gmail.com>
2023-08-22 10:09:33 +0530

    cpp

---
+++ c program/if_ragtangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,d;
+    cin >> l >> b;
+d=l*b;
+cout << d;
+if(d>45)
+{
+    cout << "right";
+} else{
+    cout << "wrong";
+}
+
+}
